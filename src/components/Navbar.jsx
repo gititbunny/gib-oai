e7e2b77010193e092a146d6ed1d2101483ed6906
Git It Bunny <ninankhwashu@gmail.com>
2026-06-11 06:22:00 +0200

    Update Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <header className="site-header">
+      <nav className="navbar container">
+        <Link to="/" className="logo">
+          <span className="logo-mark">GIB</span>
+          <span className="logo-text">Get It Bunny</span>
+        </Link>
+
+        <div className="nav-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+          <NavLink to="/process">Process</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact" className="nav-cta">
+            Start a Project
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
