208130c89e7b90f53ac2be50f85edab4bb8c24bb
Git It Bunny <gititbunny@gmail.com>
2026-07-08 03:24:05 +0200

    Update Porfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -135,7 +135,9 @@ function Portfolio() {
             </div>
           </div>
         </div>
-        <section className="section-block portfolio-categories-section">
+      </section>
+
+      <section className="section-block portfolio-categories-section">
         <div className="container">
           <div className="portfolio-category-pills">
             {categories.map((category) => (
@@ -144,9 +146,6 @@ function Portfolio() {
           </div>
         </div>
       </section>
-      </section>
-
-     
 
       <section className="section-block featured-work-section">
         <div className="container">
